047491d35b3304355cdfd573ef5de7eddeb0898d
pavittarx <29155477+pavittarx@users.noreply.github.com>
2025-04-04 01:52:16 +0000

    solved: a cloudberry jam

---
+++ contests/A_Cloudberry_Jam.cpp
@@ -0,0 +1,22 @@
+// Educational Codeforces Round 177 (Div 2)
+// Problem: https://codeforces.com/contest/2086/problem/A
+// Cloudberry Jam
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int cases;
+  cin >> cases;
+
+  while (cases != 0) {
+    int jarsCount = 0;
+    cin >> jarsCount;
+
+    cout << jarsCount * 2 << endl;
+
+    cases--;
+  }
+
+  return 0;
+}
